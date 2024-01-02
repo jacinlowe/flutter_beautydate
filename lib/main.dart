1e5bb2a7bff3f36711d6d5a39da7333f879d86a9
DESKTOP-O1CTHQ0\AL_2 <jacin.lowe@gmail.com>
2024-01-02 18:58:51 -0500

    got it all

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import '../screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
+  //  CAN SET UP A CUSTOM TOP BAR
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await windowManager.ensureInitialized();
+  // await windowManager.waitUntilReadyToShow().then((_) async {
+  //   await windowManager.setTitleBarStyle(TitleBarStyle.hidden);
+  //   await windowManager.setTitle('No title');
+  //   await windowManager.show();
+  // });
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
